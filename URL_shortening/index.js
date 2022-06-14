540c55eb0072746f2c77329c90e1ad864407fb04
Susan <98580234+susannabottelli@users.noreply.github.com>
2022-06-14 21:43:46 +0200

    Update index.js
    I have updated the code with a Mongo sanitizer to protect from injection after a GitHub dependency vulnerability alert.

--- URL_shortening/index.js
+++ URL_shortening/index.js
@@ -1,10 +1,18 @@
 import express, { json } from 'express';
 import connectDB from './config/database'; // file with database connection
+import sanitize from 'mongo-sanitize'; // TO SOLVE DEPENDENCY VULNERABILITY
 const PORT = 3000;
 const app = express();
 
 // Connect to database (here MongoDB)
 connectDB();
+
+// PROTECT FROM POSSIBLE INJECTION
+/* FROM STACK OVERFLOW: The sanitize function will strip out any keys that start
+with '$' in the input, so you can pass it to MongoDB without worrying about malicious
+users overwriting query selectors. */
+var clean = sanitize(req.params.username);
+
 
 // Accept JSON data in the API when a request to create a URL is made
 /* Some use app.use(json({ extended: false }) but I did not find documentation for an 'extended' option for .json(),
